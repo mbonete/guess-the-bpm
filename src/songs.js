e2b1c09f66106610f7e902d1da1a9ea246a94e72
maria-bonete <bonetesalmeron@gmail.com>
2022-07-22 19:29:54 +0200

    adds more songs

--- src/songs.js
+++ src/songs.js
@@ -1,11 +1,39 @@
 const songs = [
   {
+    songId: '45cYwDMibGo',
+    bpm: 83,
+  },
+  {
+    songId: 'iL-jC7XyLeo',
+    bpm: 98,
+  },
+  {
+    songId: 'I_izvAbhExY',
+    bpm: 103,
+  },
+  {
+    songId: '5NV6Rdv1a3I',
+    bpm: 116,
+  },
+  {
+    songId: '0J2QdDbelmY',
+    bpm: 124,
+  },
+  {
+    songId: 'u1xrNaTO1bI',
+    bpm: 130,
+  },
+  {
     songId: 'rY0WxgSXdEE',
     bpm: 110,
   },
   {
-    songId: '45cYwDMibGo',
-    bpm: 83,
+    songId: 'LKApCTSVppU',
+    bpm: 94,
+  },
+  {
+    songId: 'V0FoYUcq9FQ',
+    bpm: 84,
   },
   {
     songId: 'hTWKbfoikeg',
